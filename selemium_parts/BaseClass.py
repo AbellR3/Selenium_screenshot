2fc8c1206ad07d2390251329dc3c2672895a124c
Roman Mihaylov <>
2022-03-22 13:55:02 +0400

    admin init

--- selemium_parts/BaseClass.py
+++ selemium_parts/BaseClass.py
@@ -15,24 +15,6 @@ from selenium import webdriver
 
 
 
-def find_simple_image(img1:str, img2:str, name:str) -> bool:
-    image_1 = Image.open(img1).convert('RGB')
-    image_2 = Image.open(img2).convert('RGB')
-    result = ImageChops.difference(image_1, image_2)
-    if image_1.size != image_2.size:
-        return False
-    box = result.getbbox()
-
-    if box:
-        draw1 = ImageDraw.Draw(image_1)
-        draw2 = ImageDraw.Draw(image_2)
-        draw1.rectangle(result.getbbox(), fill=None, outline=(139,0,0), width = 3)
-        draw2.rectangle(result.getbbox(), fill=None, outline=(139,0,0), width = 3)
-        image_1.save(f'{name}1.png')
-        image_2.save(f'{name}2.png')
-    else:
-        return False
 
 
-find_simple_image('screenshot.png', 'screenshot2.png', 'draw')
 
