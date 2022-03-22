cf51193fa7974d307249ab40eebb4da09dd66dd2
Roman Mihaylov <>
2022-03-22 15:12:57 +0400

    create table page-elements

--- admin_parts/myadmin/models.py
+++ admin_parts/myadmin/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Page(models.Model):
+    page_title = models.CharField(max_length=600)
+    pge_url = models.URLField()
+
+    def __str__(self):
+        return self.page_title
+    
+class Element(models.Model):
+    selector_name = models.CharField(max_length=100)
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    SELECTOR = (('id', 'id'), 
+                ('class name', 'class name'),
+                ('XPATH', 'XPATH'),
+                ('tag name', 'tag name'))
+    selector = models.CharField(max_length=50, choices = SELECTOR)
+    setted_image = models.CharField(max_length=300, default=None)
+    
+    def __str__(self):
+        return self.selector_name
