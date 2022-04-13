1de97907a40c8440c06236cabc7b409fe6713f36
Roman Mihaylov <>
2022-04-13 19:01:21 +0400

    update chromedriver

--- admin_parts/myadmin/views.py
+++ admin_parts/myadmin/views.py
@@ -7,32 +7,44 @@ import os
 from myadmin.selenium_parts.BaseClass import BaseClass
 import time
 from typing import NamedTuple
+from selenium.common.exceptions import InvalidArgumentException, InvalidSelectorException
 
 
 def make_screenshot(Elements: NamedTuple , save_path: str) -> None:
+    non_correct_list = []
     for i in Elements:
         url = i.page.page_url
         selector_type = i.selector_type
         selector_text = i.selector_text
         selector_id =i.id
         new_page = ElementFinder(url, selector_type)
-        #need to add try/except
-        new_page.go_to_site()
-        #need to add try/except
-        element = new_page.find_element((new_page.selector_type, selector_text))
-        #need to add try/except
+
+        try:
+            new_page.go_to_site()
+        except InvalidArgumentException:
+            return True
+
+        try:
+            element = new_page.find_element((new_page.selector_type, selector_text))
+        except InvalidSelectorException:
+            pass
+
         element.screenshot(f'{save_path}{selector_id}.png')
 
 
 def save(request):
     Elements = Element.objects.all()
-    make_screenshot(Elements, './stable_images/' )
+    isValid = make_screenshot(Elements, './stable_images/' )
+    if isValid:
+        return HttpResponse("Can't create page object")
     return HttpResponse('all screenhots created')
 
 
 def compare_image(request):
     Elements = Element.objects.all()
-    make_screenshot(Elements, './today_images/')
+    isValid = make_screenshot(Elements, './today_images/')
+    if isValid:
+        return HttpResponse("Can't create page object")
     file_list_today = os.listdir('./today_images')
     file_list_stable = os.listdir('./stable_images')
     for fileneme in file_list_today:
